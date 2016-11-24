3ab0281e390dd03a47d628941d3c23a04115f7a2
HamzaKun <hamzakasry@gmail.com>
2016-11-24 20:50:47 +0100

    Adding project resources. Adding the readFile function.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,8 +2,30 @@
  * Created by hamza on 16/11/16.
  */
 import org.tartarus.snowball.ext.FrenchStemmer;
+
+import java.io.*;
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args){
+            //InputStream in = Test.class.getResourceAsStream("corpus-utf8/D1.html");
+            File folder = new File("target/classes/corpus-utf8");
+            File[] listOfFiles = folder.listFiles();
+            for(File tmpFile : listOfFiles) {
+                if (tmpFile.isFile()) {
+                    try {
+                        Scanner scanner = new Scanner(tmpFile);
+                        while (scanner.hasNextLine()) {
+                            String line = scanner.nextLine();
+                            System.out.println(line);
+                        }
+                        scanner.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
         String par = "intelligemment";
         FrenchStemmer frenchStemmer = new FrenchStemmer();
         frenchStemmer.setCurrent(par);
