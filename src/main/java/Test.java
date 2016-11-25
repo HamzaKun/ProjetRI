1e2defd8a149ea4e2c7beb263d296a5363444280
Phoenix <11wangyaoda@gmail.com>
2016-11-25 13:31:57 +0100

    create Jsoup Reader

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,12 +2,29 @@
  * Created by hamza on 16/11/16.
  */
 import org.tartarus.snowball.ext.FrenchStemmer;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Test {
-    public static void main(String[] args){
+    private Map table;
+    public static void main(String[] args) throws IOException {
         String par = "intelligemment";
         FrenchStemmer frenchStemmer = new FrenchStemmer();
         frenchStemmer.setCurrent(par);
         frenchStemmer.stem();
         System.out.println(frenchStemmer.getCurrent());
+        TestJsoup test = new TestJsoup();
+        test.Testfile();
+
+    }
+
+    private void ReadString(String s){
+        table = new LinkedHashMap<String,Integer>();
+        String temp[]  = s.split(" ");
+        for(String x:temp){
+            
+        }
     }
 }
