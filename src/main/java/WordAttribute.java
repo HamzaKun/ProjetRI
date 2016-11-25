11e17a7cabd9798ad6eec84b1054bbea2052d2db
Phoenix <11wangyaoda@gmail.com>
2016-11-25 16:02:47 +0100

    Create the module of WordAttribute.

--- src/main/java/WordAttribute.java
+++ src/main/java/WordAttribute.java
@@ -1,37 +1,39 @@
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by BinaryTree on 2016/11/25.
  */
 public class WordAttribute {
     private int frequency;
-    private ArrayList<String> index;
+    private Map<String, Integer> index;
 
-    public ArrayList<String> getIndex() {
+    public Map<String, Integer> getIndex() {
         return index;
     }
 
-    public void setIndex(ArrayList<String> index) {
+    public void setIndex(Map<String, Integer> index) {
         this.index = index;
     }
 
-    public WordAttribute(int frequency, ArrayList<String> index) {
+    public WordAttribute(int frequency, Map<String, Integer> index) {
         this.frequency = frequency;
         this.index = index;
+    }
+    public WordAttribute() {
+        this.frequency = 1;
+        this.index = new HashMap<String, Integer>();
     }
 
     public int getFrequency() {
         return frequency;
     }
 
-    public void setFrequency(int frequency) {
-        this.frequency = frequency;
-    }
-    public void increaseValue(){
-        this.frequency += 1;
+    public void increaseFrequencyInPath(String path){
+        this.index.put(path,this.index.get(path)+1);
     }
     public void addPath(String path){
-        this.index.add(path);
+        this.index.put(path,1);
     }
 
 }
