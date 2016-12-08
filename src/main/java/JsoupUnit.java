365c9b80281615600eee6ea2b92b831069cd5eac
HamzaKun <hamzakasry@gmail.com>
2016-12-08 17:02:16 +0100

    Adding a queriesReader.

--- src/main/java/JsoupUnit.java
+++ src/main/java/JsoupUnit.java
@@ -1,10 +1,10 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;//Element;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
 
 /**
  * Created by BinaryTree on 2016/11/16.
@@ -20,6 +20,24 @@ public class JsoupUnit {
         doc = Jsoup.parse(file,"UTF-8");
         return doc.body().text();
     }
+
+    public String readQueries(File file) throws IOException {
+        if (file == null){
+            System.err.println("NULL Document");
+            return null;
+        }
+        doc = Jsoup.parse(file, "UTF-8");
+        //Element h2Element = doc.ge;
+        Elements h2Tags = doc.getElementsByTag("h2");
+        /**
+         * To get the queries
+         */
+        for (Element tmp : h2Tags){
+            System.out.println(tmp.ownText());
+        }
+
+        return doc.body().text();
+    }
     public String ReadHtml(String url) throws IOException {
         File file = new File(url);
         if(file == null){
@@ -27,5 +45,15 @@ public class JsoupUnit {
             return null;
         }
         return ReadHtml(file);
+    }
+
+    public static void main(String[] args){
+        JsoupUnit jsoup = new JsoupUnit();
+        try {
+            File file = new File("target/classes/requetes.html");
+            System.out.println(jsoup.readQueries(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/QueriesReader.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+/**
+ * Created by hamza on 08/12/16.
+ */
+public class QueriesReader {
+
+    private JsoupUnit jsoup;
+
+    public void readQueries(String filePath) {
+        File file = new File(filePath);
+
+    }
+}
