543b61ae1f04c948b94fc33d6e08a0a935e92042
Phoenix <11wangyaoda@gmail.com>
2016-12-09 14:42:21 +0100

    Add sort.

--- src/main/java/QueryEvaluator.java
+++ src/main/java/QueryEvaluator.java
@@ -3,10 +3,7 @@ import java.io.File;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * Created by hamza on 08/12/16.
@@ -16,22 +13,29 @@ public class QueryEvaluator {
     private JsoupUnit jsoup;
     private String[] queries;
     private List<SortedMap<String, Integer>> result;
+    private static final int NUMBER_DOC = 138;
 
-    public SortedMap<String, Integer> evaluteQuery(String[] query, Connection connection) throws SQLException{
+    public SortedMap<String, Integer> evaluteQuery(String[] query, Connection connection) throws SQLException {
         SortedMap<String, Integer> queryResult = new TreeMap<String, Integer>();
-        for(String word : query) {
+        for (String word : query) {
             String sqlQuery = "Select document, frequence from RI.`index` where RI.`index`.`mot` like '%" + word + "%'";//mot like '%"+ word +"%'";
             Statement stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery(sqlQuery);
-            while(rs.next()) {
+            while (rs.next()) {
                 String document = rs.getString("document");
                 int frequence = rs.getInt("frequence");
                 queryResult.put(document, frequence);
-                System.out.println("\t( " + document + ", " + frequence +")");
+                System.out.println("\t( " + document + ", " + frequence + ")");
             }
             rs.close();
             stmt.close();
         }
+        for(int i = 1;i<= NUMBER_DOC;i++){
+            if(!queryResult.containsKey("D"+i+".html"));{
+                queryResult.put("D"+i+".html",Integer.valueOf(0));
+            }
+        }
+        queryResult = (SortedMap<String, Integer>) sortByValue(queryResult);
         return queryResult;
     }
 
@@ -40,8 +44,8 @@ public class QueryEvaluator {
             DbConnect dbConnect = new DbConnect();
             Connection connection = dbConnect.getConnection();
             result = new ArrayList<SortedMap<String, Integer>>();
-            int i=1;
-            for(String[] query : queries) {
+            int i = 1;
+            for (String[] query : queries) {
                 System.out.println("For the query :" + i++);
                 result.add(evaluteQuery(query, connection));
             }
@@ -55,5 +59,20 @@ public class QueryEvaluator {
     public void readQueries(String filePath) {
         File file = new File(filePath);
 
+    }
+
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        Map<K, V> result = new LinkedHashMap<K, V>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
     }
 }
